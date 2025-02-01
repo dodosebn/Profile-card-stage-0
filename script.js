ec7db689cdd64e8f2a45e6d553b6cf0994ac0b0c
orji Dominion <orjidominion32@gmail.com>
2025-02-01 01:53:40 +0100

    90%

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// Getting the UTC time
-const currentUTCDate = new Date();
-const utcYear = currentUTCDate.getUTCFullYear();
-const utcMonth = currentUTCDate.getUTCMonth() + 1;
-const utcDay = currentUTCDate.getUTCDate(); // Corrected to get the day of the month
-const utcHours = currentUTCDate.getUTCHours();
-const utcMinutes = currentUTCDate.getUTCMinutes();
-const utcSeconds = currentUTCDate.getUTCSeconds();
-const utcMilliSeconds = currentUTCDate.getUTCMilliseconds();
+// const socials = document.querySelector('socials');
+function updateTime() {
+    const utcTime = new Date().toUTCString();
+    document.getElementById("utc-time").textContent = `UTC Time: ${utcTime}`;
+}
 
-// Corrected formatting
-const formattedUTCDateTime = `${utcYear}-${utcMonth.toString().padStart(2, '0')}-${utcDay.toString().padStart(2, '0')} ` +
-                             `${utcHours.toString().padStart(2, '0')}:${utcMinutes.toString().padStart(2, '0')}:${utcSeconds.toString().padStart(2, '0')}.` +
-                             `${utcMilliSeconds.toString().padStart(3, '0')} UTC`;
+setInterval(updateTime, 2000);
 
-console.log(formattedUTCDateTime);
+updateTime();
